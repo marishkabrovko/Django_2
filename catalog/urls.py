fe53121dfb0c93691f4e698d2892b70715dbce93
marinabrovko <marishka.brovko@gmail.com>
2024-11-04 23:55:34 +0300

    #2

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,9 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from catalog.apps import CatalogConfig
+
+
+app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('catalog.urls', namespace='catalog'))
+    path('', )
 ]
