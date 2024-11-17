61ec4abc5d064e8f3379f85424f56a45df12c8a2
marinabrovko <marishka.brovko@gmail.com>
2024-11-17 22:18:17 +0300

    #1

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import home, contact
+from catalog.views import contacts, home, product_details, products_list
+
 
 app_name = CatalogConfig.name
 
-urlpatterns = [path("", home, name="home"), path("contacts/", contact, name="contact")]
+urlpatterns = [
+    path('home/', home, name='home'),
+    path('contacts/', contacts, name='contacts'),
+    path('', products_list, name='products_list'),
+    path('product/<int:pk>/', product_details, name='product')
+]
