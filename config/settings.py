ad240555437a2b09748136bd9ebd89d8f8db32b0
marinabrovko <marishka.brovko@gmail.com>
2024-12-16 15:02:17 +0300

    #1

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "catalog",
     "blogs",
+    "users",
 ]
 
 MIDDLEWARE = [
@@ -134,4 +135,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = os.path.join(BASE_DIR / "media")
+
+AUTH_USER_MODEL = 'users.User'
 
